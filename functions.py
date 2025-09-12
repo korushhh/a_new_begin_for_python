5117d4ad8772378467516a03bf5f3adc330504ba
K O R U S H <155060339+korushhh@users.noreply.github.com>
2025-09-12 21:26:56 +0330

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,80 @@
+def greeting(persons):
+    for name, last in persons:
+        print(f'hello {name} {last}')
+persons = [['ali', 'rezaei'], ['kamran', 'tafti'], ['reza', 'golzar']]
+greeting(persons)
+
+def odd_or_even(list):
+    res = ['even' if n%2==0 else 'odd' for n in list]
+    return res
+print(odd_or_even([1,2,3,4,5,6,7,9,11,22,44]))
+
+def files_merge(names,l_names,ages):
+    for person in zip(names,l_names,ages):
+        print(person)
+names = ['reza','majid','babak']
+l_names = ['golzar','salehi','rahnama khanande number 1 tarikh ta abad']
+ages = [48,53,41]
+
+files_merge(names,l_names,ages)
+# leet code problems
+#1
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+        return res
+#2    
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        sum1 = 0
+        place = 1
+        p = l1
+        while p is not None:
+            sum1 += p.val * place
+            place *= 10
+            p = p.next
+
+        sum2 = 0
+        place = 1
+        q = l2
+        while q is not None:
+            sum2 += q.val * place
+            place *= 10
+            q = q.next
+
+        total = sum1 + sum2
+        if total == 0:
+            return ListNode(0)
+
+        dummy = ListNode(0)
+        cur = dummy
+        while total > 0:
+            digit = total % 10
+            total //= 10
+            cur.next = ListNode(digit)
+            cur = cur.next
+
+        return dummy.next
+    
+#3
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        sum = 0
+        tmp = x
+        while(tmp != 0):
+            sum = sum * 10 + (tmp % 10)
+            tmp = int(tmp/10)
+        return sum == x
+        
