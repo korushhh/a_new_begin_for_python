2815e0c124f3e7f062983371fef81a0bc5dea5ed
K O R U S H <155060339+korushhh@users.noreply.github.com>
2025-09-13 13:50:24 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -90,4 +90,22 @@ class Solution:
                 result += vals[s[i]]
 
         return result
-        
+
+#5
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {')': '(', ']': '[', '}': '{'}
+
+        for ch in s:
+            if ch in pairs.values():
+                stack.append(ch)
+            elif ch in pairs:
+                if not stack or stack[-1] != pairs[ch]:
+                    return False
+                stack.pop()
+            else:
+                return False
+
+        return not stack
+
