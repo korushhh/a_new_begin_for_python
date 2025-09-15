e7a4e7deb94bc5500ff5c7ac0285f1c566ffbaaa
K O R U S H <155060339+korushhh@users.noreply.github.com>
2025-09-15 17:50:15 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -167,4 +167,16 @@ def tops(grades):
     return names  
 
     
+#8
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        n, m = len(haystack), len(needle)
+        if m > n:
+            return -1
+        for i in range(n - m + 1):
+            if haystack[i:i + m] == needle:
+                return i
+        return -1
 
