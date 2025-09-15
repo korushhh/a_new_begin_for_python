5f46ed03abf99029ae8c73f49965a8f58a7a0019
K O R U S H <155060339+korushhh@users.noreply.github.com>
2025-09-15 20:35:25 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -209,3 +209,13 @@ class Solution:
         num = int(''.join(map(str, digits)))
         num += 1
         return list(map(int, str(num)))
+#12
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+
+        r = x
+        while r * r > x:
+            r = (r + x // r) // 2
+        return r
