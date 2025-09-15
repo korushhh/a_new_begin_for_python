69d418186226151b31ff34a42453e83424659ecf
K O R U S H <155060339+korushhh@users.noreply.github.com>
2025-09-15 20:48:58 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -232,3 +232,22 @@ class Solution:
             memo[i] = memo[i-1] + memo[i-2]
         
         return memo[n]
+#14
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        tmp = head
+        while tmp and tmp.next:  
+            if tmp.val == tmp.next.val:  
+                tmp.next = tmp.next.next  
+            else:
+                tmp = tmp.next  
+        return head
