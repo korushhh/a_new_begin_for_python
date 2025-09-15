2a3a7181a7c4ee60f13a077124ac1bea9ab51b3b
K O R U S H <155060339+korushhh@users.noreply.github.com>
2025-09-15 20:44:36 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -214,8 +214,21 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
-
         r = x
         while r * r > x:
             r = (r + x // r) // 2
         return r
+#13 i used dynamic programming to solve this problem
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+        
+        memo = [0] * (n + 1)
+        memo[0] = 1 
+        memo[1] = 1 
+        
+        for i in range(2, n + 1):
+            memo[i] = memo[i-1] + memo[i-2]
+        
+        return memo[n]
